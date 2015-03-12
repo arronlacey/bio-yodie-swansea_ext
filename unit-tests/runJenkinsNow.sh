0c80ed1df12ccce32b6178c1041568b1264d459a
Johann Petrak <johann.petrak@gmail.com>
2015-03-12 19:09:16 +0000

    Show what we get from the server so we can spot any errors.

--- unit-tests/runJenkinsNow.sh
+++ unit-tests/runJenkinsNow.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget http://gateservice8.dcs.shef.ac.uk:18080/job/yodie-daily-unit-tests/build?token=hdjdf7d6dfdf -O /dev/null
+wget http://gateservice8.dcs.shef.ac.uk:18080/job/yodie-daily-unit-tests/build?token=hdjdf7d6dfdf -O- 
