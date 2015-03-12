475e8d0de9fa52efee7d25a1d2120a003fdef318
Johann Petrak <johann.petrak@gmail.com>
2015-03-12 19:09:39 +0000

    Make detection of difference messages in the log work.

--- unit-tests/runUnitTests.sh
+++ unit-tests/runUnitTests.sh
@@ -24,17 +24,17 @@ log=/tmp/runUnitTests$$.log
 echo Running unit tests on `date` > $log
 
 $ROOTDIR/../yodie-tools/bin/runPipeline.sh -c $SCRIPTDIR/aida-a-tuning-sample1.config.yaml  -nl -r -d -P compareAndEvaluate.xgapp ../main/main.xgapp aida-a-tuning-sample1 |& tee -a $log
-grep -v -q "=== UNIT TEST: DIFFERENCE" $log
+grep -q "=== UNIT TEST: DIFFERENCE" $log | grep -v -q "=== UNIT TEST: DIFFERENCE" 
 ret=$?
 totalret=$ret
 
 $ROOTDIR/../yodie-tools/bin/runPipeline.sh -c $SCRIPTDIR/aida-ee-sample1.config.yaml  -nl -r -d -P compareAndEvaluate.xgapp ../main/main.xgapp aida-ee-sample1 |& tee -a $log
-grep -v -q "=== UNIT TEST: DIFFERENCE" $log
+grep -q "=== UNIT TEST: DIFFERENCE" $log | grep -v -q "=== UNIT TEST: DIFFERENCE"
 ret=$?
 totalret=$((totalret + ret))
 
 $ROOTDIR/../yodie-tools/bin/runPipeline.sh -c $SCRIPTDIR/en-tweets-training-sample1.config.yaml  -nl -r -d -P compareAndEvaluate.xgapp ../main/main.xgapp en-tweets-training-sample1 |& tee -a $log
-grep -v -q "=== UNIT TEST: DIFFERENCE" $log
+grep -q "=== UNIT TEST: DIFFERENCE" $log | grep -v -q "=== UNIT TEST: DIFFERENCE"
 ret=$?
 totalret=$((totalret + ret))
 
