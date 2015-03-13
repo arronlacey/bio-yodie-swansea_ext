7f2c69b0faaa788538b1497011a6d9265b8447cf
Johann Petrak <johann.petrak@gmail.com>
2015-03-13 17:07:55 +0000

    Update to newest Evaluation plugin version and make the detection of differences work even if there are overlapping responses and reference annotations.

--- unit-tests/java/printDifferences.java
+++ unit-tests/java/printDifferences.java
@@ -8,18 +8,13 @@ public void execute() {
     int i = 0;
     for(Annotation ann : inputAS) {
       String t = ann.getType();
-      if(t.startsWith("Mention_Res") || t.startsWith("Mention_Ref")) {
-        // ignore those, these are indicator annotations for the response and reference sets
-      } else {
-        // but the rest are indicator annotations indicating change
-        String change = (String)ann.getFeatures().get("_eval.change");
-        if(change==null) { change = "??"; }
-        System.err.print("=== UNIT TEST: DIFFERENCE ("+change+") in document "+doc.getName()+" ");
-        System.err.println("for >"+gate.Utils.cleanStringFor(doc,ann)+
+      String change = (String)ann.getFeatures().get("_eval.change");
+      if(change==null) { change = "??"; }
+      System.err.print("=== UNIT TEST: DIFFERENCE ("+change+") in document "+doc.getName()+" ");
+      System.err.println("for >"+gate.Utils.cleanStringFor(doc,ann)+
           "< "+t+", ("+gate.Utils.start(ann)+
           ","+gate.Utils.end(ann)+") type="+ann.getType()+" inst="+ann.getFeatures().get("inst"));
-        i++;
-      }
+      i++;
     }
     if(i==0) {
       System.out.println("=== UNIT TEST: OK "+doc.getName());
