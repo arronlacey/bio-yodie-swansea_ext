2ed0f382cf9f101b8fecd450dd7018440bf5f678
Johann Petrak <johann.petrak@gmail.com>
2015-03-16 11:47:46 +0000

    Check in the debug documents in a separate directory, not the one where they get modified by the debug scripts.

--- unit-tests/runResetDebug.sh
+++ unit-tests/runResetDebug.sh
@@ -15,6 +15,7 @@ done
 SCRIPTDIR=`dirname "$PRG"`
 SCRIPTDIR=`cd "$SCRIPTDIR"; pwd -P`
 ROOTDIR=`cd "$SCRIPTDIR/.."; pwd -P`
-
+rm $SCRIPTDIR/debug1/*
+cp $SCRIPTDIR/debug1.in $SCRIPTDIR/debug1
 $ROOTDIR/../yodie-tools/bin/runPipeline.sh -c $SCRIPTDIR/aida-a-tuning-sample1.config.yaml -nl -d -P transferShef2Ref.xgapp ../main/main.xgapp debug1
 
