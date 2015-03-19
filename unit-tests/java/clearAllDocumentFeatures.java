fb02cecc4cdb64d3a74976dea5dd5d2592794a09
Johann Petrak <johann.petrak@gmail.com>
2015-03-19 12:02:32 +0000

    Update debug scripts and added a lot of additional info to the output if we spot a difference. Also now keep the MachineLearning sets from the reference and the testing runs.

--- unit-tests/java/clearAllDocumentFeatures.java
+++ unit-tests/java/clearAllDocumentFeatures.java
@@ -1,5 +1,33 @@
+import gate.Utils;
+import gate.*;
+import gate.util.GateRuntimeException;
+import java.util.*;
 
 @Override
 public void execute() {
+  // clear all the document features
   doc.getFeatures().clear();
+  // clear all annotation sets apart from "Shef" and "Key"
+  doc.getAnnotations().clear();
+  List<String> setnames = new ArrayList<String>();
+  setnames.addAll(doc.getAnnotationSetNames());
+  for(String setname : setnames) {
+    if(!(setname.equals("Shef") || setname.equals("Key") || setname.equals("Ref_LearningFramework"))) {
+      doc.getAnnotations(setname).clear();
+      doc.removeAnnotationSet(setname);
+    }
+  }
+  // create copies of the annotations in Shef in Ref, using the same id
+  AnnotationSet ref = doc.getAnnotations("Ref");
+  for(Annotation shef : doc.getAnnotations("Shef")) {
+    int id = shef.getId();
+    try {
+      ref.add(id,Utils.start(shef),Utils.end(shef),shef.getType(),Utils.toFeatureMap(shef.getFeatures()));    
+    } catch (Exception ex) {
+      throw new GateRuntimeException("Could not add annotation: "+shef,ex);
+    }
+  }
+  // now remove the Shef set
+  doc.getAnnotations("Shef").clear();
+  doc.removeAnnotationSet("Shef");  
 }
--- unit-tests/runDebugSingle.sh
+++ unit-tests/runDebugSingle.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+mkdir d1.afterMain1
+mkdir d1.afterTransfer
+mkdir d1.afterMain2
+mkdir d1.afterEval1
 rm d1.afterMain1/*
 rm d1.afterTransfer/*
 rm d1.afterMain2/*
@@ -7,8 +11,8 @@ rm d1.afterEval1/*
 
 runPipeline.sh -nl -c aida-a-tuning-sample1.config.yaml ../main/main.xgapp debug1 d1.afterMain1/
 
-runPipeline.sh -nl -c aida-a-tuning-sample1.config.yaml transferShef2Ref.xgapp  d1.afterMain1/ d1.afterTransfer/
+runPipeline.sh -nl  transferShef2Ref.xgapp  d1.afterMain1/ d1.afterTransfer/
 
 runPipeline.sh -nl -c aida-a-tuning-sample1.config.yaml ../main/main.xgapp   d1.afterTransfer/ d1.afterMain2/
 
-runPipeline.sh -nl -c aida-a-tuning-sample1.config.yaml compareAndEvaluate.xgapp   d1.afterMain2/ d1.afterEval1/
+runPipeline.sh -nl  compareAndEvaluate.xgapp   d1.afterMain2/ d1.afterEval1/
