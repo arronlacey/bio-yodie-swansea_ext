69605791938fab647f987d154ff0a7acb518c061
Johann Petrak <johann.petrak@gmail.com>
2015-03-20 20:33:00 +0000

    Make Pair usable in sets and add a toString implementation for debugging.

--- plugins/LodiePlugin/src/gate/trendminer/lodie/utils/Pair.java
+++ plugins/LodiePlugin/src/gate/trendminer/lodie/utils/Pair.java
@@ -17,5 +17,26 @@ public class Pair<T1,T2> {
     value1=f1;
     value2=f2;
   }
+  @Override
+  public boolean equals(Object other) {
+    if(other == null) { return false; }
+    if(!(other instanceof Pair)) { return false; }
+    if(other == this) { return true; }
+    Pair<?,?> otherPair = (Pair)other;
+    return value1.equals(otherPair.value1) && value2.equals(otherPair.value2);
+  }
+
+  @Override
+  public int hashCode() {
+    int hash = 7;
+    hash = 37 * hash + (this.value1 != null ? this.value1.hashCode() : 0);
+    hash = 37 * hash + (this.value2 != null ? this.value2.hashCode() : 0);
+    return hash;
+  }
+  
+  @Override
+  public String toString() {
+    return "Pair("+value1+","+value2+")";
+  }
   
 }
