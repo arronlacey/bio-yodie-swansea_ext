69c8fcf86e948753d20a01d7113534c0abc482fe
Johann Petrak <johann.petrak@gmail.com>
2015-03-24 18:21:21 +0000

    Evaluate based on the merged candidate lists of all lists which overlap with a key. This should not only give an estimate of maximum possible recall but also a good estimate of maximum reachable precision and f-measure.

--- unit-tests/java/maxRecall.java
+++ unit-tests/java/maxRecall.java
@@ -1,6 +1,13 @@
 // A script that uses the Evaluation plugin API to find the 
 // maximum possible recall obtainable from the candidate lists
 // we have in the lookup list annotations.
+// NOTE: this will limit the responses to just those which are overlapping
+// with the key. If more than one list is overlapping with a key, we merge 
+// the candidates of all of them into one list. The idea is that we want 
+// to find the theoritical maximum recall, but in limiting the responses to
+// just the key spots, we also want a rough estimate what the best achievable
+// precision and f-measure for the max recall situation could be.
+// The set with the filtered and merged lookup list annotations is EvalMaxRecalTmp
 
 // NOTE!!!!!
 // Since the Key annotation set contains annotations of type Mention with inst features
@@ -19,6 +26,7 @@ import gate.plugin.evaluation.resources.*;
 import java.util.*;
 import gate.*;
 import gate.util.*;
+import gate.trendminer.lodie.utils.LodieUtils;
 
 private static Set<String> featureSet = new HashSet<String>();
 static {
@@ -43,14 +51,6 @@ public void controllerStarted() {
 
 @Override
 public void controllerFinished() {
-  /*
-  EvaluateTagging.outputEvalStatsForType(
-    System.out, 
-    allStats, 
-    "Mention", 
-    "Shef", 
-    evaluationId);
-  */
   System.out.println(evaluationId+" MaxRecall Recall Strict: "+allStats.getRecallStrict());
   System.out.println(evaluationId+" MaxRecall Recall Lenient: "+allStats.getRecallLenient());
   System.out.println(evaluationId+" MaxRecall Precision Strict: "+allStats.getPrecisionStrict());
@@ -65,8 +65,8 @@ public void execute() {
   // Adapt the key annotations
   AnnotationSet key4list = doc.getAnnotations("Key4List");
   key4list.clear();
-  AnnotationSet key = doc.getAnnotations("Key").get("Mention");
-  for(Annotation ann : key) {
+  AnnotationSet keys = doc.getAnnotations("Key").get("Mention");
+  for(Annotation ann : keys) {
     // copy the annotation to the key4list set, but change the type from Mention to Lookup and
     // also change the inst feature value first
     FeatureMap fm = Utils.toFeatureMap(ann.getFeatures());
@@ -77,18 +77,44 @@ public void execute() {
     Utils.addAnn(key4list,ann,"Lookup",fm);
   }
   
+  // Now that we have the Key4List set, create the temporary response set
+  // EvalMaxRecallTmp: this will first be a copy of the original inputAS
+  // LookupList annotations, then in an second step, 
+  
   AnnotationSet listAnns = inputAS.get("LookupList");
-  System.out.println("list anns: "+listAnns.size());
+  AnnotationSet evalAnns = doc.getAnnotations("EvalMaxRecall");
+  for(Annotation keyAnn : keys) {
+    // get all the overlapping list annotations
+    AnnotationSet overlaps = Utils.getOverlappingAnnotations(listAnns,keyAnn);
+    // copy them all to the temporary set, but if there is more than one,
+    // immediately merge all others into the first
+    int firstid = -1;
+    Annotation firstAnn = null;
+    for(Annotation overlap : overlaps) {
+      int id = LodieUtils.copyListAnn(overlap,inputAS,evalAnns);
+      if(firstAnn == null) {
+        firstid = id;
+        firstAnn = evalAnns.get(firstid);
+      } else {
+        // merge the annotation with id into the annotation with firstid
+        Annotation otherAnn = evalAnns.get(id);
+        LodieUtils.mergeListAnns(evalAnns, firstAnn, otherAnn, true, "inst");
+      }
+    }
+  }
+  
+  AnnotationSet evalListAnns = evalAnns.get("LookupList");
+  
   List<CandidateList> candList =
     AnnotationDifferTagging.createCandidateLists(
-                      inputAS, 
-                      listAnns,
+                      evalAnns, 
+                      evalListAnns,
                       "ids", 
                       "relUriFreqByLabelInWp");
   System.out.println("candidate lists: "+candList.size());                     
   AnnotationDifferTagging differ = AnnotationDifferTagging.calculateEvalStatsTagging4List(
     key4list,
-    listAnns,
+    evalListAnns,
     candList,
     featureSet,
     fcmp,
