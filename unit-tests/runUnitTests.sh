9e975bf2ce72d3014ea9fc055ff7034e70f66e84
Johann Petrak <johann.petrak@gmail.com>
2015-03-24 16:52:12 +0000

    Store all the logs and data files in the logs subdirectory in the unit-tests directory. The user can choose which of those are relevant and should get checked in.

--- unit-tests/runUnitTests.sh
+++ unit-tests/runUnitTests.sh
@@ -15,12 +15,14 @@ done
 SCRIPTDIR=`dirname "$PRG"`
 SCRIPTDIR=`cd "$SCRIPTDIR"; pwd -P`
 ROOTDIR=`cd "$SCRIPTDIR/.."; pwd -P`
+LOGDIR="$SCRIPTDIR/logs"
 
 totalret=0
 
 ts=`date +%Y%m%d_%H%M%S`
 
-log=/tmp/runUnitTests-${ts}.log
+mkdir $LOGDIR
+log=$LOGDIR/runUnitTests-${ts}.log
 echo Running unit tests on `date` > $log
 
 ## create the output directories if not already there and empty them
@@ -31,7 +33,7 @@ rm $SCRIPTDIR/aida-ee-sample1.out/* >& /dev/null
 mkdir $SCRIPTDIR/en-tweets-training-sample1.out >& /dev/null
 rm $SCRIPTDIR/en-tweets-training-sample1.out/* >& /dev/null
 
-outDir=/tmp
+outDir=$LOGDIR
 outUrl=file://${outDir}
 
 evalId=runUnitTests-aida-a-tuning-${ts}
@@ -54,28 +56,22 @@ totalret=$((totalret + ret))
 
 function summary() {
   ts=$1 
-  grep $ts $ROOT/runUnitTest-$ts.log | grep 'MaxRecall Recall'
-  grep $ts $ROOT/runUnitTest-$ts.log | grep 'th=none' | grep 'F1.0' | sed -e "s/.\+\.log://" -e "s/, type=Mention, th=none,//"
-  grep $ts $ROOT/runUnitTest-$ts.log | grep 'th=none' | grep 'Precision' | sed -e "s/.\+\.log://" -e "s/, type=Mention, th=none,//"
-  grep $ts $ROOT/runUnitTest-$ts.log | grep 'th=none' | grep 'Recall' | sed -e "s/.\+\.log://" -e "s/, type=Mention, th=none,//"
+  grep $ts $log | grep 'MaxRecall Recall'
+  grep $ts $log | grep 'th=none' | grep 'F1.0' | sed -e "s/.\+\.log://" -e "s/, type=Mention, th=none,//"
+  grep $ts $log | grep 'th=none' | grep 'Precision' | sed -e "s/.\+\.log://" -e "s/, type=Mention, th=none,//"
+  grep $ts $log | grep 'th=none' | grep 'Recall' | sed -e "s/.\+\.log://" -e "s/, type=Mention, th=none,//"
 }
 
 if [ $totalret != 0 ]
 then
-  cp $log $ROOTDIR/runUnitTest-$ts.log
-  cp $outDir/EvaluateTagging-runUnitTests-*-$ts.tsv $ROOTDIR/
-  echo 'UNIT TEST DIFFERENCES!' Log is in $ROOTDIR/runUnitTest-$ts.log, data files are in $ROOTDIR/EvaluateTagging-runUnitTests-*-$ts.tsv
+  echo 'UNIT TEST DIFFERENCES!' Log is in $LOGDIR/runUnitTest-$ts.log, data files are in $LOGDIR/EvaluateTagging-runUnitTests-*-$ts.tsv
   grep "=== UNIT TEST: DIFFERENCE" $log
   summary $ts
-  echo 'UNIT TEST DIFFERENCES!' Log is in $ROOTDIR/runUnitTest-$ts.log
-  rm $log
-  rm $outFile
+  echo 'UNIT TEST DIFFERENCES!' Log is in $LOGDIR/runUnitTest-$ts.log
   exit 1
 else 
-  cp $log $ROOTDIR/runUnitTest-$ts.log
-  cp $outFile/EvaluateTagging-runUnitTests-*-$ts.tsv $ROOTDIR/
   summary $ts
-  echo 'UNIT TEST COMPLETED WITHOUT DIFFERENCES! Log is in' $ROOTDIR/runUnitTest-$ts.log, data files are in $ROOT/EvaluateTagging-runUnitTests-*-$ts.tsv
+  echo 'UNIT TEST COMPLETED WITHOUT DIFFERENCES! Log is in' $LOGDIR/runUnitTest-$ts.log, data files are in $LOGDIR/EvaluateTagging-runUnitTests-*-$ts.tsv
   rm $log
   rm $outFile
 fi
