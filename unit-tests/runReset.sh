5df3253beaadc804bae37e8739ef2063ef405c5f
Johann Petrak <johann.petrak@gmail.com>
2015-04-19 18:18:12 +0100

    Change unit test scripts so that they use the main.config.yaml settings.

--- unit-tests/runReset.sh
+++ unit-tests/runReset.sh
@@ -16,8 +16,21 @@ SCRIPTDIR=`dirname "$PRG"`
 SCRIPTDIR=`cd "$SCRIPTDIR"; pwd -P`
 ROOTDIR=`cd "$SCRIPTDIR/.."; pwd -P`
 
-$ROOTDIR/../yodie-tools/bin/runPipeline.sh -c $SCRIPTDIR/aida-a-tuning-sample1.config.yaml -nl -d -P transferShef2Ref.xgapp ../main/main.xgapp aida-a-tuning-sample1
+## NOTE: all scripts use the main.config.yaml config now but those parts which need to be
+## set to different values are specified via the command line (so far, only twitter
+## related settings)
 
-$ROOTDIR/../yodie-tools/bin/runPipeline.sh -c $SCRIPTDIR/aida-ee-sample1.config.yaml -nl -d -P transferShef2Ref.xgapp ../main/main.xgapp aida-ee-sample1
+$ROOTDIR/../yodie-tools/bin/runPipeline.sh -c $ROOTDIR/main/main.config.yaml -nl -d -P transferShef2Ref.xgapp $ROOTDIR/main/main.xgapp aida-a-tuning-sample1
 
-$ROOTDIR/../yodie-tools/bin/runPipeline.sh -c $SCRIPTDIR/en-tweets-training-sample1.config.yaml -nl -d -P transferShef2Ref.xgapp ../main/main.xgapp en-tweets-training-sample1
+$ROOTDIR/../yodie-tools/bin/runPipeline.sh -c $ROOTDIR/main/main.config.yaml -nl -d -P transferShef2Ref.xgapp $ROOTDIR/main/main.xgapp aida-ee-sample1
+
+## NOTE: this overrides the following two settings from the main.config.yaml file:
+##- set: docfeature
+##  name: docType
+##  value: tweet
+##- set: prrun
+##  controller: filter-prescoring
+##  prname: Java:projectTwitterUserID
+##  value: true
+
+$ROOTDIR/../yodie-tools/bin/runPipeline.sh -c $ROOTDIR/main/main.config.yaml -Dmodularpipelines.docfeature.docType=tweet -Dmodularpipelines.prrun.filter-prescoring.Java:projectTwitterUserID=true -nl -d -P transferShef2Ref.xgapp $ROOTDIR/main/main.xgapp en-tweets-training-sample1
