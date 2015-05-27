30cebe66c114570cc354ddbad11b42954d131ff4
Johann Petrak <johann.petrak@gmail.com>
2015-05-27 20:09:09 +0100

    Make it work after we removed the unit-test set specific config files.

--- unit-tests/runDebugSingle.sh
+++ unit-tests/runDebugSingle.sh
@@ -11,10 +11,10 @@ rm d1.afterEval1/*
 
 runPipeline.sh -nl resetAllButKey.xgapp debug1
 
-runPipeline.sh -nl -c aida-a-tuning-sample1.config.yaml ../main/main.xgapp debug1 d1.afterMain1/
+runPipeline.sh -nl -c ../main/main.config.yaml  ../main/main.xgapp debug1 d1.afterMain1/
 
 runPipeline.sh -nl  transferShef2Ref.xgapp  d1.afterMain1/ d1.afterTransfer/
 
-runPipeline.sh -nl -Dmodularpipelines.prrun.lookupinfo.Java:copyListAnns=true  -c aida-a-tuning-sample1.config.yaml ../main/main.xgapp   d1.afterTransfer/ d1.afterMain2/
+runPipeline.sh -nl -Dmodularpipelines.prrun.lookupinfo.Java:copyListAnns=true  -c ../main/main.config.yaml ../main/main.xgapp   d1.afterTransfer/ d1.afterMain2/
 
 runPipeline.sh -nl -DmaxRecall.evalId=DEBUG -Dmodularpipelines.prparm.compareAndEvaluate.Evaluate.evaluationId=DEBUG -Dmodularpipelines.prparm.compareAndEvaluate.Evaluate.outputDirectoryUrl=file:///tmp  compareAndEvaluate.xgapp   d1.afterMain2/ d1.afterEval1/
