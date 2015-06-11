7ed7480cffda4661d3375fa6dce28e92c9aae486
Genevieve Gorrell <g.gorrell@sheffield.ac.uk>
2015-06-11 12:34:06 +0100

    High frequency candidate is added even if ML rejects it.

--- disambiguation-libsvm/groovy/SelectBestCandidate.groovy
+++ disambiguation-libsvm/groovy/SelectBestCandidate.groovy
@@ -5,7 +5,7 @@
 AnnotationSet defaultAS = doc.getAnnotations();
 
 def confidenceThresholdString = System.getProperty("lodie.disambiguation-libsvm.SelectBestCandidate.confidenceThreshold")
-def confidenceThreshold = -0.75
+def confidenceThreshold = 0.0
 if(confidenceThresholdString != null) {
   confidenceThreshold = confidenceThresholdString.toDouble()
 }
@@ -13,7 +13,9 @@ if(confidenceThresholdString != null) {
 defaultAS.get("LookupList").each{ ll ->
     AnnotationSet candidates = Utils.getCoextensiveAnnotations(inputAS, ll, "Mention_disamb");
     Annotation bestcandidate = null;
+    Annotation commonestCandidate = null;
     double scoreofbestcandidate = -1.0F;
+    int uriFreqOfCommonestCandidate = -1;
     
     //Compete the candidates
     for(Annotation cand : candidates){
@@ -31,10 +33,22 @@ defaultAS.get("LookupList").each{ ll ->
         scoreofbestcandidate = candidatescore;
       }
 
+      int candidatefreq = -1;
+      if (cand.getFeatures().get("uriFreqInWp")!=null){
+        candidatefreq = (int)cand.getFeatures().get("uriFreqInWp");
+      }
+
+      if(candidatefreq>uriFreqOfCommonestCandidate || commonestCandidate==null){
+        commonestCandidate = cand;
+        uriFreqOfCommonestCandidate = candidatefreq;
+      }
+
     }
 
     if(bestcandidate!=null && scoreofbestcandidate>confidenceThreshold){
       gate.Utils.addAnn(outputAS, bestcandidate, "Mention", Utils.toFeatureMap(bestcandidate.getFeatures()));
+    } else if(commonestCandidate!=null && uriFreqOfCommonestCandidate>1000){
+      gate.Utils.addAnn(outputAS, commonestCandidate, "Mention", Utils.toFeatureMap(commonestCandidate.getFeatures()));
     //} else {
     // TODO: check if we should create a NIL annotation here!
     //  gate.Utils.addAnn(outputAS, ll, "Mention", Factory.newFeatureMap());
