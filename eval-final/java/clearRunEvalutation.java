b8f8ee0cedd5ee9d7ea19ff2949da87e716cd648
Johann Petrak <johann.petrak@gmail.com>
2015-08-11 22:59:42 +0200

    Add the subpipeline for the final in-pipeline evaluation.

---
+++ eval-final/java/clearRunEvalutation.java
@@ -0,0 +1,4 @@
+@Override
+public void execute() {
+  doc.getFeatures().remove("runEvaluation");
+}
---
+++ eval-final/java/printEvalErrors.java
@@ -0,0 +1,11 @@
+
+/**
+ * From the various annotation sets with indicator annotations, 
+ * create a tsv file that lists the most interesting ones.
+ * Currently we show the following errors:
+ * 
+ */
+@Override
+public void execute() {
+  
+}
