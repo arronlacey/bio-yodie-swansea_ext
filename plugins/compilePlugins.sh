0366b586fab727ce9fea55ca88803ad2f46dbe8e
Genevieve Gorrell <g.gorrell@sheffield.ac.uk>
2016-03-03 10:33:29 +0000

    Added disambiguation plugin.

--- plugins/compilePlugins.sh
+++ plugins/compilePlugins.sh
@@ -26,7 +26,7 @@ SCRIPTDIR=`dirname "$PRG"`
 SCRIPTDIR=`cd "$SCRIPTDIR"; pwd -P`
 pushd "$SCRIPTDIR"
 tmpout=/tmp/`whoami`-compilePlugins$$.out
-for file in Evaluation ModularPipelines JdbcLookup TwitterExpander Learning_Framework YodiePlugin Java
+for file in Evaluation ModularPipelines JdbcLookup TwitterExpander Learning_Framework YodiePlugin Java Disambiguation
 do
   if [ "$file" == ANNIE ] || [ ! -d "$file" ]
   then
